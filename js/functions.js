aba918d675097a69fbe396439b694c01fcea00ed
Juan Pablo <jdufourg09@gmail.com>
2023-02-09 23:43:59 -0300

    desarrollo de funciones javascript, y agregado de botones

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+const hexadecimal = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+
+function aleatorio(inferior,superior){
+    let numPosibilidades = superior - inferior
+    let aleat = Math.random() * numPosibilidades
+    aleat = Math.floor(aleat)
+    return parseInt(inferior) + aleat
+ } 
+
+function generar_color_aleatorio(){
+    let color_aleatorio = "#";
+    for (i=0;i<6;i++){
+        //falta definir la funcion aleatorio
+       let posarray = aleatorio(0,hexadecimal.length);
+       color_aleatorio += hexadecimal[posarray];
+    }
+    return color_aleatorio;
+ }
+
+
+document.getElementById("botonColorDeFondo").addEventListener("click", function(){
+    document.getElementById("contenidoCv").style.backgroundColor = generar_color_aleatorio();
+    console.log("cambiamos de color el fondo");
+})
