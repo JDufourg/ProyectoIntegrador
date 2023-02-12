ebcc8cc6671619d3df8951059fc4e28896377517
Juan Pablo <jdufourg09@gmail.com>
2023-02-12 13:46:42 -0300

    Configuracion de compoprtamiento responsivo para dispositivos pequeños

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 const hexadecimal = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
 function aleatorio(inferior,superior){
-    let numPosibilidades = superior - inferior
-    let aleat = Math.random() * numPosibilidades
-    aleat = Math.floor(aleat)
-    return parseInt(inferior) + aleat
- } 
+    let numPosibilidades = superior - inferior;
+    let aleat = Math.random() * numPosibilidades;
+    aleat = Math.floor(aleat);
+    return parseInt(inferior) + aleat;
+ };
 
 function generar_color_aleatorio(){
     let color_aleatorio = "#";
@@ -15,10 +15,13 @@ function generar_color_aleatorio(){
        color_aleatorio += hexadecimal[posarray];
     }
     return color_aleatorio;
- }
+ };
 
-
+//boton cambiar de color de fondo a uno aleatorio.
 document.getElementById("botonColorDeFondo").addEventListener("click", function(){
     document.getElementById("contenidoCv").style.backgroundColor = generar_color_aleatorio();
     console.log("cambiamos de color el fondo");
-})
+});
+
+
+
